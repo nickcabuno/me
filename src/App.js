1bded9a112dc44fbe4df7d17ce058a9b4ff86d74
nickcabuno <nickcabuno17@gmail.com>
2021-08-19 09:55:42 -0400

    under construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import construction from './assets/underconstructionwebsite.gif'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi im typically really good with this kind of thing but i've just been a little busy
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <img alt="construction" src={construction}/>
+        </div>
+        <p className="bottom">
+          since you're here, you are probably someone i'm trying to impress
+        </p> 
+        <p className="bottom">
+          so instead of showing you an unfinished, underwhelming site i will simply show you this stupid gif
+        </p>
+        <p className="bottom">if you'd like to see some actual work i've done: <a href="https://github.com/nickcabuno">GitHub</a>
+        </p>
+        <p className="bottom">
+          if you'd like to ask me about why this still isn't finished yet: <a href="https://www.linkedin.com/in/nick-cabuno/">LinkedIn</a>
+        </p>
     </div>
   );
 }
